980c76bc68624ecf346cc5e50f7a2259fe8e07c6
Ok0iSUV52ZSsLnD <contateste1910101@gmail.com>
2025-11-05 20:36:11 -0300

    Create Themes.lua

---
+++ Themes.lua
@@ -0,0 +1,26 @@
+local Themes = {
+  ["Default"] = {
+    ["Color Hub 1"] = Color3.fromRGB(18, 18, 18),
+    ["Color Hub 2"] = Color3.fromRGB(25, 25, 25),
+    ["Color Theme"] = {
+    Type = "Gradient",
+    Color = ColorSequence.new{
+        ColorSequenceKeypoint.new(0, Color3.fromRGB(255, 0, 0)),
+        ColorSequenceKeypoint.new(1, Color3.fromRGB(0, 0, 255))
+    }
+    },
+    ["Color Text"] = Color3.fromRGB(240, 240, 240),
+    ["Color Dark Text"] = Color3.fromRGB(200, 200, 200),
+    ["Color Stroke"] = Color3.fromRGB(80, 80, 80),
+    ["Corner Radius"] = UDim.new(0, 5),
+    ["Font"] = {
+      Enum.Font.GothamBlack,
+      Enum.Font.GothamBold,
+      Enum.Font.GothamMedium
+    },
+    ["Stroke Thickness"] = 1,
+    ["Tween Style"] = Enum.EasingStyle.Quad
+  },
+}
+
+return Themes
